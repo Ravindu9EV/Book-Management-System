6ec0b6328b6875100df255a6b0d5578bc3d7f930
Yasantha Ravindu <99851400+Ravindu9EV@users.noreply.github.com>
2024-11-16 00:10:57 +0530

    book management system

---
+++ .idea/book_management_system/Main.dart
@@ -0,0 +1,45 @@
+enum Status {available,borrowed}
+class Book{
+  String? title;
+  String? author;
+  String? isbn;
+  bool stts;
+
+
+  Book(this.title,this.author,this.isbn,this.stts);
+  Book.empty():
+        title="",
+        author="",
+        isbn="",
+        stts=false;
+
+
+  setTitle(String title)=>this.title=title;
+  setAuthor(String author)=>this.author=author;
+  setIsbn(String isbn)=>this.isbn=isbn;
+  setStatus(bool statuss){if(statuss){
+    this.stts=true;
+  }
+  }
+
+  String? getTitle() => this.title;
+
+  String? getAuthor()=> this.author;
+  String? getIsbn()=>this.isbn;
+  bool? get status=>this.stts;
+
+  void diaplayInfo(){
+    String? s="Title: $title\nAuthor: $author\nISBN: %s\nAvailability: $stts";
+    print(s);
+  }
+
+}
+class TextBook extends Book{
+  TextBook(String? title,String? author,String? isbn,bool status):super(title,author,isbn,status);
+
+}
+void main() {
+  Book textBook=new TextBook("Text Book 1","Yasantha","978-2-6778-21-123",true);
+  textBook.diaplayInfo();
+  print("hi");
+}
